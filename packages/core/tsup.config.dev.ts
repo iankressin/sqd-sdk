467ea9252ecf37017b05f7557b6c73cd5199dd58
iankressin <ianguimaraes31@gmail.com>
2025-07-31 19:04:40 -0300

    chore: add development build scripts and configurations for core and solana-stream packages

---
+++ packages/core/tsup.config.dev.ts
@@ -0,0 +1,23 @@
+import {globSync} from 'glob'
+import {defineConfig} from 'tsup'
+import {copyFileSync} from 'node:fs'
+
+const entries = globSync('src/**/*.ts', {ignore: ['src/**/*.test.ts']})
+
+export default defineConfig({
+    entry: entries,
+    outDir: 'lib',
+    format: ['cjs'],
+    bundle: false,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    outExtension({format}) {
+        return {
+            js: format === 'cjs' ? '.cjs' : '.js',
+        }
+    },
+    async onSuccess() {
+        copyFileSync('package.json', 'lib/package.json')
+    },
+})
---
+++ packages/solana/solana-stream/tsup.config.dev.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'tsup'
+import {globSync} from 'glob'
+import {copyFileSync} from 'node:fs'
+
+const entries = globSync('src/**/*.ts', {ignore: ['src/**/*.test.ts']})
+
+export default defineConfig({
+    entry: entries,
+    outDir: 'lib',
+    format: ['cjs'],
+    bundle: true,
+    clean: true,
+    splitting: false,
+    sourcemap: true,
+    outExtension({format}) {
+        return {
+            js: format === 'cjs' ? '.cjs' : '.js',
+        }
+    },
+    async onSuccess() {
+        copyFileSync('package.json', 'lib/package.json')
+    },
+})
